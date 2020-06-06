2b5ecf1db98f908d4b4f0e9e8457aa5df2bfaa5a
Isaac Nemzer <ijnemzer@outlook.com>
2020-06-06 02:20:34 -0400

    Refactor file structure

--- index.js
+++ index.js
@@ -3,35 +3,22 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import express from 'express';
 import fs from 'fs';
-// import Debug from 'debug';
-// const debug
-// var debug = require('debug')('MyApp');
 
-import Meta from './src/js/components/Meta.js';
-import getFacts from './src/js/facts.js';
+import Meta from './server/js/components/Meta.js';
+import getFacts from './server/js/facts.js';
 
 const index = fs.readFileSync(__dirname + '/index.html', 'utf8')
 
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(`you in here b? prob not`)
   getFacts().then(facts => {
-    console.log(`you in here b?`)
     const metaHtml = renderToString(<Meta facts={facts} />)
-    console.log(index)
-    console.log('~~~~~~~~~~~~~~~~')
-    console.log(metaHtml)
     const finalHtml = index.replace('<!-- ::META:: -->', metaHtml)
-    console.log('~~~~~~~~~~~~~~~~')
-    console.log(finalHtml)
     res.send(finalHtml);
   });
 });
 
 app.use(express.static('.'));
-
-const port = 3000;
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 export let gsvrisk = functions.https.onRequest(app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: './server/index.js',
   module: {
     rules: [
       {
